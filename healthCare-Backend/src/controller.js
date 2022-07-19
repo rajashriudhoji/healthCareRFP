bb0f69ad18fd83a30bd1b6f38f95c880f82941f4
avnishexcellarate <99027559+avnishexcellarate@users.noreply.github.com>
2022-07-19 14:32:59 +0530

    Creating others table of pateint details. (#5)

--- healthCare-Backend/src/controller.js
+++ healthCare-Backend/src/controller.js
@@ -15,18 +15,78 @@ const addPatientFollowUpAppointments = (patient_id, folowUpAppointments) => {
     if (error) throw error;
     return result;
   });
+};
 
+const addPatientVisit = (patient_id, patientVisit) => {
+  const {dateOfService, vitalSigns, smokeStatus} = patientVisit;
+
+  pool.query(queries.addPatientVisit, [patient_id, dateOfService, vitalSigns, smokeStatus], (error, result) => {
+    if (error) throw error;
+    return result;
+  });
+};
+
+const addPatientBreastFeeding = (patient_id, patientBreastFeeding) => {
+
+  const {isBreastfeeding, feedLength, feedFrequency, supplimentFormula,
+    feedingComfort, isNippleCracked} = patientBreastFeeding;
+
+  pool.query(queries.addPatientBreastFeeding, [patient_id, isBreastfeeding, feedLength,
+    feedFrequency, supplimentFormula, feedingComfort, isNippleCracked], (error, result) => {
+      if (error) throw error;
+      return result;
+  });
+};
+
+const addPatientSafeSpacing = (patient_id, patientSafeSpacing) => {
+  const {birthControl, birthControlAssess} = patientSafeSpacing;
+
+  pool.query(queries.addPatientSafeSpacing, [patient_id, birthControl, birthControlAssess], (error, result) => {
+    if (error) throw error;
+    return result;
+  });
+};
+
+const addPatientPsychoSocialAssess = (patient_id, patientPsychoSocialAssess) => {
+  const {relationWithBaby, houseMemberStatus, fatherStatus,
+    safety, unsafeRelationStatus, resourceStatus} = patientPsychoSocialAssess;
+
+  pool.query(queries.addPatientPsychoSocialAssess, [patient_id, relationWithBaby, houseMemberStatus, fatherStatus,
+    safety, unsafeRelationStatus, resourceStatus], (error, result) => {
+      if (error) throw error;
+      return result;
+  });
+};
+
+const addPatientEducationalMaterial = (patient_id, patientEducationalMaterial) => {
+  const {depressionScreening, contraceptionMethod, peripheralBloodGlucose,
+    doctorAppointment, carSeatSafety, immunizationSchedule, breastFeeding,
+    infantSafety, familyPlanning, checkups, details} = patientEducationalMaterial;
+
+  pool.query(queries.addPatientEducationalMaterial, [patient_id, depressionScreening, contraceptionMethod, peripheralBloodGlucose,
+    doctorAppointment, carSeatSafety, immunizationSchedule, breastFeeding,
+    infantSafety, familyPlanning, checkups, details], (error, result) => {
+      if (error) throw error;
+      return result;
+  });
 };
 
 const addPatient = async(req, res) => {
   try{
-    const {motherName, babyName, babyDOB, address, email, phone, babyGender, folowUpAppointments} = req.body;
+    const {patientBasicInfo:{motherName, babyName, babyDOB, address, email, phone, babyGender},
+      folowUpAppointments, patientVisit, patientBreastFeeding, patientSafeSpacing,
+      patientPsychoSocialAssess, patientEducationalMaterial} = req.body;
 
     //TODO: write middleware to validate input.
     const patient = await pool.query(queries.addPatient, [motherName, babyName, babyDOB, address, email, phone, babyGender]);
     const patient_id = patient.rows.length ? patient.rows[0].patient_id : 0;
     if(patient_id){
       addPatientFollowUpAppointments(patient_id, folowUpAppointments);
+      addPatientVisit(patient_id, patientVisit);
+      addPatientBreastFeeding(patient_id, patientBreastFeeding);
+      addPatientSafeSpacing(patient_id, patientSafeSpacing);
+      addPatientPsychoSocialAssess(patient_id, patientPsychoSocialAssess);
+      addPatientEducationalMaterial(patient_id, patientEducationalMaterial);
     }
 
     res.json({
--- healthCare-Backend/src/database.sql
+++ healthCare-Backend/src/database.sql
@@ -20,38 +20,24 @@ CREATE TABLE patientFollowUpAppointments (
 CREATE TABLE patientVisit (
 	patient_id NUMERIC(5) NOT NULL,
 	dateOfService DATE,
-	abdomen json,
 	vitalSigns json,
-	babyDOB DATE
-);
-
-CREATE TABLE patientClinicalAssess (
-	patient_id NUMERIC(5) NOT NULL,
-	smokeStatus BOOLEAN,
-	neuro json,
-	skin json,
-	lungs json,
-	abdomen json,
-	elimination json,
-	perimeum json,
-	lochia json,
-	pain json,
+	smokeStatus BOOLEAN
 );
 
 CREATE TABLE patientBreastFeeding (
 	patient_id NUMERIC(5) NOT NULL,
 	isBreastfeeding BOOLEAN,
 	feedLength NUMERIC(5),
-	sfeedFrequencykin NUMERIC(5),
+	feedFrequency NUMERIC(5),
 	supplimentFormula BOOLEAN,
 	feedingComfort BOOLEAN,
-	isNippleCracked BOOLEAN,
+	isNippleCracked BOOLEAN
 );
 
 CREATE TABLE patientSafeSpacing (
 	patient_id NUMERIC(5) NOT NULL,
 	birthControl json,
-	birthControlAssess json,
+	birthControlAssess json
 );
 
 CREATE TABLE patientPsychoSocialAssess (
@@ -61,10 +47,10 @@ CREATE TABLE patientPsychoSocialAssess (
 	fatherStatus json,
 	safety json,
 	unsafeRelationStatus json,
-	resourceStatus json,
+	resourceStatus json
 );
 
-CREATE TABLE patientClinicalAssess (
+CREATE TABLE patientEducationalMaterial (
 	patient_id NUMERIC(5) NOT NULL,
 	depressionScreening json,
 	contraceptionMethod json,
@@ -76,5 +62,5 @@ CREATE TABLE patientClinicalAssess (
 	infantSafety json,
 	familyPlanning json,
 	checkups json,
-	details VARCHAR(355),
+	details VARCHAR(355)
 );
