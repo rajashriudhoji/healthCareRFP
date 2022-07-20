d2037a5ba77ef1f2981ae03c27934be09c5bab35
avnishexcellarate <avnish.kumar@excellarate.com>
2022-07-20 12:25:35 +0530

    API to get patient details by patient id.

--- healthCare-Backend/src/routes.js
+++ healthCare-Backend/src/routes.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get('/', controller.getPatient);
 router.post('/', controller.addPatient);
+router.get('/:patient_id', controller.getPatientById);
 
 module.exports = router;
