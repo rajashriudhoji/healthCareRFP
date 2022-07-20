a97d69d49da00af7c2c61773019a96805196c22e
Rutuja Dhumal <rutuja.dhumal@excellarate.com>
2022-07-20 13:38:08 +0530

    Updated getSelectedValue

--- healthCare-Frontend/src/utils/functions.js
+++ healthCare-Frontend/src/utils/functions.js
@@ -1,12 +1,3 @@
 export const getSelectedValue = (object) => {
-  let key = "";
-  Object.keys(object).some((item) => {
-    const isValueTrue = object[item] === true;
-    if (isValueTrue) {
-      key = item;
-    }
-    return isValueTrue;
-  });
-  console.log({ object }, { key });
-  return key;
+  return Object.keys(object).filter((item) => object[item] === true);
 };
